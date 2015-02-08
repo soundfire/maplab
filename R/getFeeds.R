eb0575b3ebc49599e5cd0e055dc2a4be9e4d5209
soundfire <mjm1363@gmail.com>
2015-02-08 12:36:43 +0100

    Conflicts resolved

--- R/getFeeds.R
+++ R/getFeeds.R
@@ -9,9 +9,5 @@ getFeeds=function(input=input){
   lists=input[1:(length(input)-1)]
   lists=LIST[is.element(names(LIST),lists)]
   lists=gsub("feed://","http://",lists)
-  list(channel=paste(lists,collapse=""))
-<<<<<<< HEAD
-=======
-  # list(channel="yeh")
->>>>>>> cae97cb0eafcee5445dbc7d2aa4b7c097d5d9865
+  list(channel=paste(lists,map,collapse=""))
 }
