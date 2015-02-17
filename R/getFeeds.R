cec6752ecd0f2572871c43f252acb209cd014515
soundfire <mjm1363@gmail.com>
2015-02-18 00:07:23 +0100

    Conflicts resolved

--- R/getFeeds.R
+++ R/getFeeds.R
@@ -159,8 +159,8 @@ getFeeds=function(input=input){
   json='[{"type":"FeatureCollection","features":['
   for (i in hits){
     crit=docs$Docs$match==i
-    pops=paste(paste("<a href='",docs$Docs$adrs[crit],"'target='_blank'>",
-                     as.character(docs$Docs$title[crit]),"</a>",sep=""),collapse="<br><br>")
+    pops=paste(paste("<a href='",docs$Docs$adrs[crit],"' target='_blank'>'",
+                     as.character(docs$Docs$title[crit]),"'</a>",sep=""),collapse="<br><br>")
     type=length(sPDF@polygons[[i]]@Polygons)
     if (type==1){
       xy=sPDF@polygons[[i]]@Polygons[[1]]@coords
